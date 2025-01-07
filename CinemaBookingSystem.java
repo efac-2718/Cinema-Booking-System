ee86077c381eba73b24505145c249c205b6ab861
M V S Peiris <peirism-ec22070@stu.kln.ac.lk>
2025-01-07 18:25:42 +0530

    Create CinemaBookingSystem.java

---
+++ CinemaBookingSystem.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class CinemaBookingSystem {
+
+    private ArrayList<Movie> movies ;
+    private Movie sampleMovie1;      //This is created only for the purpose of initialisation and to avoid error of empty collection
+
+    public CinemaBookingSystem(){
+        this.movies.add(sampleMovie1);
+
+    }
+
+    public void addMovie(Movie movie){
+        movies.add(movie);
+    }
+
+    public Movie findMovie(String name){
+        for(Movie s: movies){
+            if(s.getName().equals(name)){
+                System.out.println("The movie "+ name + " Exists");
+                return s;
+            }
+        }
+        System.out.println("The movie does not exist");
+        return null;
+    }
+
+    public void removeMovie(String name){
+        movies.remove(findMovie(name));
+    }
+}

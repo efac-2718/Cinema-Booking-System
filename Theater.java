b0cc911d14791476a576ccf96df52e9dc26056ae
M V S Peiris <peirism-ec22070@stu.kln.ac.lk>
2025-01-16 22:19:50 +0530

    Create Theater.java

---
+++ Theater.java
@@ -0,0 +1,18 @@
+public class Theater {
+
+    private String name;
+    private String location;
+    private int numberOfScreens;
+
+
+    public Theater(String name,String location){
+        this.name = name;
+        this.location = location;
+    }
+
+    public Theater(){
+        this.name = "Sample Theater";
+        this.location = "Sample location";
+    }
+
+}
